9114468ebc3686f8163130e583fb9eb8684c2ad1
Connor Goldberg <connor@connorgoldberg.com>
2017-04-14 10:11:19 -0400

    Added SPI interface abstraction

--- pi/common.py
+++ pi/common.py
@@ -25,3 +25,12 @@ def flatten(lst):
 			l.append(item)
 	return l
 
+def reverseBits(byte):
+	"""
+	Reverses the bits in an 8-bit byte
+	"""
+	byte = ((byte & 0xF0) >> 4) | ((byte & 0x0F) << 4)
+	byte = ((byte & 0xCC) >> 2) | ((byte & 0x33) << 2)
+	byte = ((byte & 0xAA) >> 1) | ((byte & 0x55) << 1)
+	return byte
+
