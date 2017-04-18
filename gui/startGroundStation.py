b82eee6c7f3d695512523c075843ad29ff8b9bd0
Connor Goldberg <cjg3259@rit.edu>
2017-04-18 13:21:32 -0400

    Made main script that will start up soundmodem, logger, GUIs on meteor lab pc

--- gui/startGroundStation.py
+++ gui/startGroundStation.py
@@ -12,6 +12,19 @@ time.sleep(1)
 
 # Start logger to parse axlisten log to get data/commands out of it from the platform
 subprocess.Popen(["python","axlisten_logger.py"])
+time.sleep(1)
+
+# Start command GUI to send commands to the platform
+subprocess.Popen(["python","command_gui.py"])
+time.sleep(1)
+
+# Start data display GUI to show graphs of sensor data
+subprocess.Popen(["python","data_plot_gui.py"])
+time.sleep(1)
+
+# Start GPS data display GUI to show GPS data path (GPS data from the telemetry stream)
+subprocess.Popen(["python","gps_data_gui.py"])
+time.sleep(1)
 
 while True:
     time.sleep(1)
